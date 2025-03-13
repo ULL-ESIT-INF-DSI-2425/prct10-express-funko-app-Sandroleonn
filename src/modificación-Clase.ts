e220a2490a0b4cc65618fc02d3641e810a8490a2
Sandro León <alu0101560250@ull.edu.es>
2025-03-13 10:39:56 +0000

    Ejercicios clase

--- src/modificación-Clase.ts
+++ src/modificación-Clase.ts
@@ -1,4 +1,4 @@
-import { PrintableCollection} from "../src/ejercicio1-modi"
+import { PrintableCollection } from "../src/ejercicio1-modi"
 
 /**
    * Subclase que contiene un método para imprimir elementos númericos
